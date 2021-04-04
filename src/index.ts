a8b724e3704b1d7892e302169944a3c14144e11d
iph9x <iphnx9@gmail.com>
2021-04-05 02:27:49 +0700

    config panel fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './styles/main.scss';
+import './styles/configPanel.scss';
 import './ts/miSlider.ts';
 
 type Props = {
@@ -16,7 +17,7 @@ type Props = {
   }
 }
 
-$('.slider').miSlider({
+const localState1: Props = {
   min: 500,
   max: 1200,
   defaultMin: 500,
@@ -24,21 +25,12 @@ $('.slider').miSlider({
   range: true,
   step: 5,
   labels: true,
-  vertical: true
-});
-
-$('.slider-2').miSlider({
-  min: 700,
-  max: 1200,
-  defaultMin: 600,
-  defaultMax: 1000,
-  step: 1,
-  labels: true,
+  vertical: true,
   inputsId: {
-    inputFromId: 'first-input',
-    inputToId: 'second-input'
-  }
-});
+    inputFromId: 'first-input-1',
+    inputToId: 'second-input-1'
+  },
+}
 
 const localState2: Props = {
   min: 700,
@@ -53,36 +45,60 @@ const localState2: Props = {
   },
 }
 
-
+$('.slider').miSlider(localState1);
+$('.slider-2').miSlider(localState2);
 
 function setSliderPanelHandlers(panelName: string, sliderName: string, state: Props): void {
   $(panelName).find('.slider-panel__step').val(state.step ? state.step : 1);
+  $(panelName).find('.slider-panel__min').val(state.min);
+  $(panelName).find('.slider-panel__max').val(state.max);
 
   $(panelName).on('change', '.slider-panel__step', function () {
     state.step = Number($(this).val());
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
     $(sliderName).miSlider(state);
   });
-  // $(panelName).on('change', '.slider-panel__min', function () {
-  //   state.min = Number($(this).val());
-  //   $(sliderName).miSlider(state);
-  // });
-  // $(panelName).on('change', '.slider-panel__max', function () {
-  //   state.max = Number($(this).val());
-  //   $(sliderName).miSlider(state);
-  // });
-  
+  $(panelName).on('change', '.slider-panel__min', function () {
+    state.min = Number($(this).val());
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
+    $(sliderName).miSlider(state);
+  });
+  $(panelName).on('change', '.slider-panel__max', function () {
+    state.max = Number($(this).val());
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
+    $(sliderName).miSlider(state);
+  });
+  $(panelName).on('change', '.slider-panel__current-from', function () {
+    state.defaultMin = Number($(this).val());
+    $(sliderName).miSlider(state);
+  });
+  $(panelName).on('change', '.slider-panel__current-to', function () {
+    state.defaultMax = Number($(this).val());
+    $(sliderName).miSlider(state);
+  });  
   $(panelName).on('change', '.slider-panel__range', function () {
     state.range = this.checked;
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
+
     $(sliderName).miSlider(state);
   });
   $(panelName).on('change', '.slider-panel__labelsVisibility', function () {
     state.labels = this.checked;
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
     $(sliderName).miSlider(state);
   });
   $(panelName).on('change', '.slider-panel__vertical', function () {
     state.vertical = this.checked;
+    state.defaultMin = Number($(panelName).find('.slider-panel__current-from').val());
+    state.defaultMax = Number($(panelName).find('.slider-panel__current-to').val());
     $(sliderName).miSlider(state);
   });
 }
 
 setSliderPanelHandlers('.slider-panel-2', '.slider-2', localState2);
+setSliderPanelHandlers('.slider-panel-1', '.slider', localState1);
